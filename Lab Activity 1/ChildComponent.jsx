ed992c8d94f35639506cf1cdda52e2bc125ea44e
hanneka <98503316+hanneka@users.noreply.github.com>
2024-01-09 14:59:13 +0800

    Add files via upload

---
+++ Lab Activity 1/ChildComponent.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+/*function ChildComponent(props) {
+    return (
+        <div>
+            <p>{props.message}</p>
+        </div>
+    );
+}
+*/
+
+class ChildComponent extends React.Component{
+    render() {
+        return (
+            <div>{this.props.message}</div>
+        );
+    }
+}
+
+export default ChildComponent;
---
+++ Lab Activity 1/Welcome.jsx
@@ -0,0 +1,13 @@
+//Hanneka Rose Puri
+import React from "react";
+
+export default class Welcome extends React.Component{
+    render () {
+        return ( <div>
+            <h2>I'm Hanneka Puri!</h2>
+            <p>Welcome to my system</p>
+            <p>Can you make our system?</p>
+        </div>
+        );
+    }
+}
