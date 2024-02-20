17a3cf763ef9dca9c9f91b6637b899a3292c1079
hanneka <98503316+hanneka@users.noreply.github.com>
2024-02-20 15:13:37 +0800

    Add files via upload

---
+++ Solidity Basics II Demo/ifElse.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract IfElse {
+    constructor() {
+    }
+
+    function foo(uint x) public pure returns (uint) {
+        if (x < 10) {
+            return 0;
+        } else if (x < 20) {
+            return 1;
+        } else {
+            return 2;
+        }
+    }
+
+    function ternary(uint _x) public pure returns (uint) {
+        return _x < 10 ? 1 : 2;
+    }
+}
