d9721b5d587397fc47575ca5a8b604a97fd2f374
hanneka <98503316+hanneka@users.noreply.github.com>
2024-04-15 17:41:18 +0800

    Add files via upload

---
+++ Finals/interact.js
@@ -0,0 +1,22 @@
+const alchemyKey = "wss://eth-sepolia.g.alchemy.com/v2/0s_QIOIG_zZmktxyyffhBbN773auieib"
+const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
+const web3 = createAlchemyWeb3(alchemyKey);
+
+
+//export const helloWorldContract;
+
+export const loadCurrentMessage = async () => { 
+  
+};
+
+export const connectWallet = async () => {
+  
+};
+
+export const getCurrentWalletConnected = async () => {
+  
+};
+
+export const updateMessage = async (address, message) => {
+  
+};
